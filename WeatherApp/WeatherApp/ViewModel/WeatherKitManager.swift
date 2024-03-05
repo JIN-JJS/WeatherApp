7ba68392b8b15bea8fbef438d7ea99982ff69182
JIN-JJS <joonsu2005@naver.com>
2024-03-05 19:47:24 +0900

    [Add] WeatherKitManager swift file add error

---
+++ WeatherApp/WeatherApp/ViewModel/WeatherKitManager.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherKitManager.swift
+//  Gilgaon
+//
+//  Created by 전준수 on 3/5/24.
+//
+
+import Foundation
+import WeatherKit
+
+@MainActor class WeatherKitManager: ObservableObject {
+}
