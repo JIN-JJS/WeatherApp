17128a56cc903d22b6ee39c534d216e8255071e2
Joonsu Jeon <114235515+JIN-JJS@users.noreply.github.com>
2024-03-05 17:58:52 +0900

    [Add] Create Project
    Uploade repositories

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 전준수 on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
